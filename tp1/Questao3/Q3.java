b952498373f50c15160caf50fe55cdd83dfcabd8
nathan <1659795@PLU4PLFLL130314.pucminas.net>
2026-08-26 12:07:36 -0300

    Questao4

---
+++ tp1/Questao3/Q3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Q3{
+	public static void main(String[] args){
+
+	}
+	public static boolean ehpalavra(String string, int tam){
+
+	}
+	public static boolean ehreal(String string, int tam){
+		
+	}
+	public static boolean 
+
+
+
